37db27288de9c8906188b67278286bd8337f1f0b
mxmgn <mxmgnchrv@gmail.com>
2023-12-10 18:32:53 +0200

    update

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='w-full pl-40 pt-6'>
-            <h3 className='text-2xl font-bold'>Main</h3>
+            <h3 className='text-2xl font-medium'>Главная</h3>
         </div>
     );
 };
